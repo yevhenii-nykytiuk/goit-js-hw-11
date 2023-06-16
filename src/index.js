9bc5c99521e2def43ae22e0703306286f604b92e
Yevhenii Nykytiuk <crashbandicoot123r@gmail.com>
2023-06-16 18:45:41 +0300

    added apdate code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Notiflix from "notiflix";
-import { getElGallery } from "./script/api";
+import { fetchImages } from "./script/api";
+
 
 
 const searchGalleryForm = document.getElementById("search-form");
@@ -7,85 +8,125 @@ const galleryMarkup = document.querySelector(".gallery");
 const loadBtnGallery = document.querySelector(".load-more");
 
 let page = 1;
+let perPage = 5;
 let searchValue = "";
 
 searchGalleryForm.addEventListener("submit", hendleFormSearchGallery);
 loadBtnGallery.addEventListener("click", hendlePageLoadMore);
 
 
+function createGallery(pictures) {
+
+    if (!galleryMarkup) {
+      return;
+    }
+
+  const markup = pictures.map(picture => {
+
+    const {
+
+      webformatURL,
+      tags,
+      likes,
+      views,
+      comments,
+      downloads
+
+    } = picture;
+
+      return `<div class="photo-card">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" width="322" height="226"/>
+        <div class="info">
+          <p class="info-item">
+          <b>Likes ${likes}</b>
+        </p>
+        <p class="info-item">
+          <b>Views ${views}</b>
+        </p>
+        <p class="info-item">
+          <b>Comments ${comments}</b>
+        </p>
+        <p class="info-item">
+          <b>Downloads ${downloads}</b>
+        </p>
+        </div>
+     </div>`
+    
+    }).join("");
+
+   galleryMarkup.insertAdjacentHTML("beforeend", markup);
+  } 
+
+
+
 function hendleFormSearchGallery(e) {
 
   e.preventDefault();
 
-  const form = e.currentTarget;
-  const searshQuery = form.elements.searchQuery.value.trim();
-
+  const searchQuery = e.currentTarget.elements.searchQuery.value.trim();
   page = 1;
-
-  searchValue = searshQuery;
-
+  searchValue = searchQuery
   clearMarkup()
+  hideBtnLoadMore();
 
-  getElGallery(searchValue).then(({ hits }) => {
-    if (searchValue.length === 0) {
-      throw new Error("Not data!");
-    }
+  if (searchValue === "") {
+    onError();
+  }
 
-    return hits.reduce((markup, hit) => { markup + getMarkupImages(hit) }, "")
-    
-  }).catch(onError);
+  fetchImages(searchValue, page, perPage).then(({data}) => {
+
+    createGallery(data.hits);
+    showBtnLoadMore()
+
+  }).catch((error) => console.log(error));
 
   e.currentTarget.reset();
 }
 
 
 
-function getMarkupImages({webformatURL, tags, likes, views, comments, downloads}) {
-  galleryMarkup.insertAdjacentHTML("afterbegin", 
-  `<div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" width="322" height="226"/>
-  <div class="info">
-    <p class="info-item">
-      <b>Likes ${likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views ${views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments ${comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads ${downloads}</b>
-    </p>
-  </div>
-</div>`);
-  
-}
 
 function hendlePageLoadMore() {
 
   page += 1;
 
-  getElGallery(searchValue, page)
-  fetchMarkup();
+  fetchImages(searchValue, page, perPage).then(({ data }) => {
+    createGallery(data.hits);
+  });
+
 }
 
-function fetchMarkup() {
-  then(({ hits }) => {
-    if (searchValue.length === 0) {
-      throw new Error("Not data!");
-    }
-    return hits.reduce((markup, hit) => { markup + getMarkupImages(hit) }, "")   
-  }).catch(onError);
-}
+
+
 
 function clearMarkup() {
   galleryMarkup.innerHTML = "";
 }
 
+
 function onError() {
   throw new Error(Notiflix.Notify.failure('Qui timide rogat docet negare'));
 }
+
+
+
+function disabledBtnLoadMore() {
+  loadBtnGallery.setAttribute("disabled", false);
+}
+
+function enabledBtnLoadMore() {
+  loadBtnGallery.setAttribute("disabled", true);
+}
+
+function showBtnLoadMore() {
+  loadBtnGallery.style.display = "block";
+}
+
+function hideBtnLoadMore() {
+  loadBtnGallery.style.display = "none";
+}
+
+
 
 
 
--- src/script/api.js
+++ src/script/api.js
@@ -1,12 +1,13 @@
+import axios from "axios";
+
 const API_KEY = "37254887-1c916d48415972de0f5a632e0";
 const API_URL = "https://pixabay.com/api/";
 
-function getElGallery(searchId, page = 1) {
+function fetchImages(searchId, page = 1, perPage = 5) {
 
-  return fetch(`${API_URL}?key=${API_KEY}&q=${searchId}&image_type=photo&orientation=horizontal&safesearch=true&per_page=5&page=${page}`).then((res) => res.json());
+  return axios.get(`${API_URL}?key=${API_KEY}&q=${searchId}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${page}`);
 
 }
 
 
-
-export {getElGallery};
+export {fetchImages};
